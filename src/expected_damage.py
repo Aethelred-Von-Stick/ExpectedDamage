869e642bd634dede1838943b4bec44f4d77860c3
Aethelred-Von-Stick <samwalkerhall@gmail.com>
2022-02-22 22:15:35 +0000

    added a test for find_damage and improved the function

--- src/expected_damage.py
+++ src/expected_damage.py
@@ -15,24 +15,34 @@ def find_depth(depth_arr, fraction):
     # estimate the depth of water at the risk.
     return depth_arr.mean() * fraction
 
-def find_damage(exp_depth, vuln_curve):
-    # Find the damage bin that the estimated depth lies in.
-    mask = logical_and(vuln_curve[:,0] < exp_depth, vuln_curve[:,1] >= exp_depth)
-    # Output the amount of damage corresponding to the estimated depth.
-    Damage_index = mask.nonzero()
-    return vuln_curve[Damage_index,2].item()
+def find_damage(exp_depth, vuln_curve, quiet = True):
+    # first check that the expected depth lies within the vulnerability curve's validity
+    if exp_depth <= vuln_curve[-1,1]:
+        # Find the damage bin that the estimated depth lies in.
+        Damage_index = logical_and(vuln_curve[:,0] < exp_depth, vuln_curve[:,1] >= exp_depth).nonzero()
+        # Output the amount of damage corresponding to the estimated depth.
+        Damage = vuln_curve[Damage_index,2].item()
+        if quiet == False:
+            print('The expected damage is £' + format(Damage, ".2f") + '.')
+        return vuln_curve[Damage_index,2].item()
+    else:
+        Damage = vuln_curve[-1,2].item()
+        if quiet == False:
+            print("The expected depth lies outwith the validity of the vulnerability curve. Using highest estimate of £" + format(Damage, "2f") + ".")
+        return Damage
+
 
 def main():
     # The paths to the data.
-    depths_file = 'depths.csv'
-    vulnerability_curve_file = 'vulnerability_curve.csv'
+    depths_file = '../data/depths.csv'
+    vulnerability_curve_file = '../data/vulnerability_curve.csv'
 
     # The fraction of the postcode area inundated by water.
     fraction_inundated = 0.75
     
     depths, vulnerability_curve = read_files(depths_file, vulnerability_curve_file)
     expected_depth = find_depth(depths, fraction_inundated)
-    print('The expected damage is £' + format(find_damage(expected_depth, vulnerability_curve), ".2f") + '.')
+    Damage = find_damage(expected_depth, vulnerability_curve, quiet = False)
 
 if __name__ == "__main__":
     main()    
